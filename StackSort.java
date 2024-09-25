16f231bc47ed0594c13c7d9e2ffdf070f5038c98
SonuKumar <djsonukumarkushwaha@gmail.com>
2024-09-25 09:23:47 +0530

    25 sept 2024

---
+++ StackSort.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class StackSort {
+    public static void sort(Stack<Integer> stack) {
+        Stack<Integer> tempStack = new Stack<>();
+
+        while (!stack.isEmpty()) {
+            int temp = stack.pop();
+
+            while (!tempStack.isEmpty() && tempStack.peek() > temp) {
+                stack.push(tempStack.pop());
+            }
+
+            tempStack.push(temp);
+        }
+
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(3);
+        stack.push(31);
+        stack.push(98);
+        stack.push(92);
+        stack.push(23);
+
+        System.out.println("Original Stack: " + stack);
+
+        sort(stack);
+
+        System.out.println("Sorted Stack: " + stack);
+    }
+}
