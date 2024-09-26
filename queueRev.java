0e412583f4bff233684ec6f97f36f47d517e1ba4
SonuKumar <djsonukumarkushwaha@gmail.com>
2024-09-26 12:11:38 +0530

    26 sept 2024

---
+++ queueRev.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class queueRev {
+  public static void main(String[] args) {
+    Queue<Integer> q = new ArrayDeque<Integer>();
+    q.add(5);
+    q.add(6);
+    q.add(3);
+    q.add(7);
+
+    System.out.println("Original Queue:");
+    printQueue(q);
+
+  
+    Queue<Integer> reversedQueue = reverseQueue(q);
+
+    System.out.println("Reversed Queue:");
+    printQueue(reversedQueue);
+  }
+
+  public static Queue<Integer> reverseQueue(Queue<Integer> queue) {
+    Stack <Integer> stack = new Stack<>();
+
+    while (!queue.isEmpty()) {
+      stack.push(queue.poll());
+    }
+
+    while (!stack.isEmpty()) {
+      queue.add(stack.pop());
+    }
+
+    return queue; 
+  }
+
+  public static void printQueue(Queue<Integer> queue) {
+    Queue<Integer> tempQueue = new ArrayDeque<>(queue);
+    while (!tempQueue.isEmpty()) {
+      System.out.print(tempQueue.poll() + " ");
+    }
+    System.out.println();
+  }
+}
